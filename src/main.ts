30221e2cb75a8530a371bafcc89870b69d354c3a
gaspard <gaspard@gmail.com>
2025-04-22 08:26:10 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const usersWithPosts = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.dir(usersWithPosts, { depth: null });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
